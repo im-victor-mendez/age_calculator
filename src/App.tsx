e3e2a8b75c55db07a07a08d7c304abd82d075264
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-06-23 13:10:32 -0600

    feat: Implemented calculator function

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,47 @@
 import { ReactComponent as ArrowIcon } from '@/assets/svg/icon-arrow.svg'
 import { useState } from 'react'
 
-type InputDate = {
+type ObjectDate = {
 	day: number
 	month: number
 	year: number
 }
 
 function App() {
-	const [date, setDate] = useState<InputDate>()
+	const [date, setDate] = useState<ObjectDate>()
+	const [calculatedDate, setCalculatedDate] = useState<ObjectDate>()
+	const [error, setError] = useState(false)
 
 	function handleInput(event: { target: { value: string; name: string } }) {
 		const value = event.target.value
 		const name = event.target.name
-		setDate({ ...date, [name]: parseInt(value) } as InputDate)
+		setError(false)
+		setDate({ ...date, [name]: parseInt(value) } as ObjectDate)
 	}
 
 	function calculate(event: { preventDefault: () => void }) {
 		event.preventDefault()
+
+		const valueIsUndefined = (value: number | undefined) => value == undefined
+		if (!date) return setError(true)
+		if (Object.values(date).some(valueIsUndefined)) return setError(true)
+
+		const inputDate = new Date(date.year, date.month, date.day)
+		const currentDate = new Date()
+		const difference = currentDate.getTime() - inputDate.getTime()
+
+		const millisecondsPerYear = 1000 * 60 * 60 * 24 * 365.25
+		const years = Math.floor(difference / millisecondsPerYear)
+		const millisecondsPerMonth = millisecondsPerYear / 12
+		const months = Math.floor(
+			(difference % millisecondsPerYear) / millisecondsPerMonth
+		)
+		const millisecondsPerDay = 1000 * 60 * 60 * 24
+		const days = Math.floor(
+			(difference % millisecondsPerMonth) / millisecondsPerDay
+		)
+
+		setCalculatedDate({ day: days, month: months, year: years })
 	}
 
 	return (
@@ -52,7 +76,11 @@ function App() {
 					</button>
 				</form>
 				<div className="divider"></div>
-				<article id="results"></article>
+				<article id="results">
+					<p>{calculatedDate?.year} years</p>
+					<p>{calculatedDate?.month} months</p>
+					<p>{calculatedDate?.day} days</p>
+				</article>
 			</main>
 		</>
 	)
